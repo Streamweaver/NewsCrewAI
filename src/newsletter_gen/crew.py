0621d575826ff6c2847f961a08447b8b25879798
Scott Turnbull <streamweaver@gmail.com>
2024-08-11 11:00:37 -0400

    Additional Codebase Documentation Added.

--- src/newsletter_gen/crew.py
+++ src/newsletter_gen/crew.py
@@ -1,6 +1,7 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from newsletter_gen.tools.research import SearchAndContents, FindSimilar, GetContents
+from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_groq import ChatGroq
 from datetime import datetime
@@ -20,7 +21,8 @@ class NewsletterGenCrew:
     tasks_config = "config/tasks.yaml"
 
     def llm(self):
-        llm = ChatAnthropic(model_name="claude-3-sonnet-20240229", max_tokens=4096)
+        llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.7, api_key=os.getenv("OPENAI_API_KEY"))
+        # llm = ChatAnthropic(model_name="claude-3-sonnet-20240229", max_tokens=4096)
         # llm = ChatGroq(model="llama3-70b-8192")
         # llm = ChatGroq(model="mixtral-8x7b-32768")
         # llm = ChatGoogleGenerativeAI(google_api_key=os.getenv("GOOGLE_API_KEY"))
